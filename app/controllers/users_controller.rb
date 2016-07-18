a1c499573008ce157158f35cdd9009ed29240b01
nidhirajkotia <nidhi.b999@gmail.com>
2016-07-18 16:05:48 +0530

    Update users_controller.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -28,7 +28,7 @@ class UsersController < ApplicationController
 
     respond_to do |format|
       if @user.save
-	  UserMailer.registration_confirmation(@user).deliver_now
+	  #UserMailer.registration_confirmation(@user).deliver_now
         format.html { redirect_to @user, notice: 'User was successfully created.' }
         format.json { render :show, status: :created, location: @user }
       else
